b0f404977e05ffddc497c1e5c42485891abc9dd3
Marlon <marmoneymar@icloud.com>
2019-12-29 21:38:36 -0500

    Initial Commit - App setup
    - Did some setup in App Delegate as this is all programmatic.
    - Added assets
    - Deleted things we don't need.

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Marlon Raskin on 12/29/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemPink
+	}
+
+
+}
+
