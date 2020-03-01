3b192122031c899b3ef9f51f0200222471fe01de
Marlon <marmoneymar@icloud.com>
2020-03-01 15:28:58 -0500

    (Added) Info Header view + New label class

--- Screens/UserInfoVC.swift
+++ Screens/UserInfoVC.swift
@@ -10,24 +10,78 @@ import UIKit
 
 class UserInfoVC: UIViewController {
 
+	let headerView = UIView()
+	let itemViewOne = UIView()
+	let itemViewTwo = UIView()
+
 	var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		configureViewController()
+		layoutUI()
+		getUserInfo()
+    }
+
+
+	func configureViewController() {
 		view.backgroundColor = .systemBackground
 		let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
 		navigationItem.rightBarButtonItem = doneButton
+	}
 
+
+	func getUserInfo() {
 		NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
 			guard let self = self else { return }
+
 			switch result {
 			case .success(let user):
-				print(user)
+				DispatchQueue.main.async {
+					self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+				}
 			case .failure(let error):
-				self.presentGFAlertOnMainThread(title: "Somethingwent wrong", message: error.rawValue, buttonTitle: "OK")
+				self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
 			}
 		}
-    }
+	}
+
+
+	func layoutUI() {
+		let padding: CGFloat = 20
+		let itemHeight: CGFloat = 140
+
+		view.addSubviews(headerView, itemViewOne, itemViewTwo)
+		[headerView, itemViewOne, itemViewTwo].forEach {
+			$0.translatesAutoresizingMaskIntoConstraints = false
+
+			NSLayoutConstraint.activate([
+				$0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+				$0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+			])
+		}
+
+
+		NSLayoutConstraint.activate([
+			headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			headerView.heightAnchor.constraint(equalToConstant: 180),
+
+			itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+			itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+
+			itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+			itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+		])
+	}
+
+
+	func add(childVC: UIViewController, to containerView: UIView) {
+		addChild(childVC)
+		containerView.addSubview(childVC.view)
+		childVC.view.frame = containerView.bounds
+		childVC.didMove(toParent: self)
+	}
+
 
 	@objc func dismissVC() {
 		dismiss(animated: true)
